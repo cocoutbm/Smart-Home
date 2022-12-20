edb003a5d0635245addbc0a131cad5836f508783
cocoutbm <coco.durupt@gmail.com>
2022-12-20 12:17:17 +0100

    Premier commit du projet

---
+++ Smart-Home-code/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import Smart_Home_Joueur.*;
+import Smart_Home_Syteme.*;
+
+public class Main {
+    public static void main(String [] args){
+
+    }
+}
---
+++ Smart-Home-code/src/main/java/Smart_Home_Joueur/Joueur.java
@@ -0,0 +1,4 @@
+package Smart_Home_Joueur;
+
+public class Joueur {
+}
