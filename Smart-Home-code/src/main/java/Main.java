1f2ba8cc32c2c61c04f86f3dba30052a6bffc22a
cocoutbm <83784422+cocoutbm@users.noreply.github.com>
2022-12-24 13:53:21 +0100

    Delete Smart-Home-code directory

--- Smart-Home-code/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import Smart_Home_Joueur.*;
-import Smart_Home_Syteme.*;
-
-public class Main {
-    public static void main(String [] args){
-
-    }
-}
--- Smart-Home-code/src/main/java/Smart_Home_Joueur/Joueur.java
+++
@@ -1,4 +0,0 @@
-package Smart_Home_Joueur;
-
-public class Joueur {
-}
