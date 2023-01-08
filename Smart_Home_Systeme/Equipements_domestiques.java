6994aff7bb783cbdaab4496e5649481107678059
woubraim <113429930+woubraim@users.noreply.github.com>
2023-01-08 21:22:19 +0100

    Update Equipements_domestiques.java

--- Smart_Home_Systeme/Equipements_domestiques.java
+++ Smart_Home_Systeme/Equipements_domestiques.java
@@ -1,10 +1,12 @@
 package Smart_Home_Systeme;
 
+//class qui definit un équipement domestique
 public class Equipements_domestiques {
-    protected String nom;
-    protected double consommation;
-    private boolean estUtilise;
+    protected String nom;                           //nom de l'équipement
+    protected double consommation;                  //valeur de la consommation de l'équipement
+    private boolean estUtilise;                     //valeur pour savoir si un équipement est utilis (TRUE/FALSE)
 
+//methode permettant de génere un equipement par défault  
     public Equipements_domestiques(){
         this.nom="ED";
         this.consommation=0.0;
