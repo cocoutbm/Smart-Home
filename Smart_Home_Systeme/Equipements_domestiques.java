c3dcc0fd52e7d291dad654e7b3ab00b34e21e03e
woubraim <113429930+woubraim@users.noreply.github.com>
2023-01-08 21:36:05 +0100

    Update Equipements_domestiques.java

--- Smart_Home_Systeme/Equipements_domestiques.java
+++ Smart_Home_Systeme/Equipements_domestiques.java
@@ -6,34 +6,40 @@ public class Equipements_domestiques {
     protected double consommation;                  //valeur de la consommation de l'équipement
     private boolean estUtilise;                     //valeur pour savoir si un équipement est utilis (TRUE/FALSE)
 
-//methode permettant de génere un equipement par défault  
+//methode permettant de génerer un equipement par défault  
     public Equipements_domestiques(){
         this.nom="ED";
         this.consommation=0.0;
         this.estUtilise = false;
     }
+    
+ //methode permettant de génerer un equipement selon un nom, une consommation et un etat d'utilisation 
     public Equipements_domestiques(String nom, float consommation, boolean use) {
         this.nom = nom;
         this.consommation = consommation;
         this.estUtilise = use;
     }
-
+    
+//methode permettant de rentrer un objet utilisé ou non utilisé
     public void ustiliserEquipementsDomestiques(boolean use) {
-        this.estUtilise = use;
+        this.estUtilise = use;  //on change l'etat d'utilisation selon le parametre
     }
-
+    
+//methode permettant d'afficher un équipement domestique (affiche son nom et sa consommation)
     public void showEquipementsDomestiques() {
         System.out.println("    nom: " + nom + ", consommation: " + consommation + ".");
     }
 
+ //methode permettant de récuperer la consommation d'un equipement
     public double getConsommation() {
-        if (estUtilise){
+        if (estUtilise){                //si il est utilisé on retourne sa conso
             return consommation;
         } else {
-            return 0;
+            return 0;                   //si il est pas utilisé on retourne 0
         }
     }
-
+    
+//methode permettant de retourner l'etat d'un equipement (savoir si il est utilisé ou non)
     public boolean isEstUtilise(){
         return estUtilise;
     }
